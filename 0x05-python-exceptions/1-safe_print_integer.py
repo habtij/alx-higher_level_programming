1f80233d5a5d389e570bfee17f170f51f75906f2
Habeeb <tijanihabeeb12@outlook.com>
2023-03-29 10:47:56 +0100

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_integers(value):
+def safe_print_integer(value):
     try:
-        print("{:d}."format(value))
+        print("{:d}".format(value))
         return True
-    except TypeError:
+    except ValueError:
         return False
