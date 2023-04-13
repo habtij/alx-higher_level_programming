105d3daa8642086559b4f6dec3f334fc0a61b478
Habeeb <tijanihabeeb12@outlook.com>
2023-04-14 00:51:54 +0100

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@
 def add_integer(a, b=98):
     """return the sum of a and b"""
 
-    if a is None or (float(a) != a or int(a) != a):
+    if a is None or (type(a) is not float or type(a) is not int):
         raise TypeError("a must be an integer")
     elif float(b) != b or int(b) != b:
         raise TypeError("b must be an integer")
