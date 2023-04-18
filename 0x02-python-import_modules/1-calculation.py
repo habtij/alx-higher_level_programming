a48200eb8ed5f34938f0a168b2c8cf5fd7c6d476
Habeeb <tijanihabeeb12@outlook.com>
2023-04-18 21:03:52 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
+
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
