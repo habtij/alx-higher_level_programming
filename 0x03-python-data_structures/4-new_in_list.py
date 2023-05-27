e0322655771c6691745af4a4b435a9cd74f6d0c3
Habeeb <tijanihabeeb12@outlook.com>
2023-05-27 16:46:49 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0:
+        return(None)
+    elif idx > len(my_list) - 1:
+        return(None)
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+    new_list[idx] = element
+    return(new_list)
