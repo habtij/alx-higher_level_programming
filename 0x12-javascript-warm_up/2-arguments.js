f3df92d61edc1420052aa39fd1c328e1462aa049
Habeeb <tijanihabeeb12@outlook.com>
2023-06-14 03:06:27 +0100

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+import { argv } from 'node:process';
+
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('Argument found');
+}
