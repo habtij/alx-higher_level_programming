98d58565fbc30de7f27dba07d0c84b7dbbd5731d
Habeeb <tijanihabeeb12@outlook.com>
2023-06-14 11:38:49 +0100

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 const { argv } = require('process');
 
-function factorial(n) {
+function factorial (n) {
   if (isNaN(Number(n)) || Number(n) === 0 || Number(n) === 1) {
-    return(1);
+    return (1);
   }
-  return(Number(n) * factorial(Number(n) - 1));
+  return (Number(n) * factorial(Number(n) - 1));
 }
 
 console.log(factorial(argv[2]));
