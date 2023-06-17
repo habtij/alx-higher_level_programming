6e9e772f14ad4151d90f578e5af8c2d9cd5b1ff7
Habeeb <tijanihabeeb12@outlook.com>
2023-06-17 08:46:32 +0100

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -24,4 +24,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__self = size
+        self.__size = size
