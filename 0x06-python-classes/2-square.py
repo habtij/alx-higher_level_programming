f21fcfc7c56d5817506a4dcfa7b13a750f7b186f
Habeeb <tijanihabeeb12@outlook.com>
2023-06-17 08:11:50 +0100

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" contains 'Square' class and '__size' attribute """
+
+
+class Square:
+    """ A Square class with private attribute size """
+    __size
+
+    def __init__(self, size=0):
+        """ An initializer with size attribute """
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__self = size
