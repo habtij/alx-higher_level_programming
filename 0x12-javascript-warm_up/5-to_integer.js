983ca52a892f0e13497f001ba6cc73dd97517589
Habeeb Tijani <tijanihabeeb12@outlook.com>
2023-08-07 17:48:22 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = require('process');
-
-if (Number(argv[2])) {
-  console.log('My number: ' + Math.floor(Number(argv[2])));
-} else {
-  console.log('Not a number');
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = require('process');
-
-function add (a, b) {
-  console.log(a + b);
-}
-
-add(Number(argv[2]), Number(argv[3]));
