c98c0cafe334912cf8e7ac8f7bdf0ed6c5ed5133
Habeeb Tijani <tijanihabeeb12@outlook.com>
2023-08-07 17:41:24 +0000

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/10-add.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-def add(a, b):
-    """returns the addition of a and b"""
-    return (a + b)
--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-i = 0
-for ch in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(ch - i)), end="")
-    if i == 0:
-        i = 32
-    else:
-        i = 0
