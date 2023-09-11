daf4128115de9b76e270ddbd843ca1e985e88f1d
Habeeb <tijanihabeeb12@outlook.com>
2023-09-11 13:20:33 +0100

    Add: 5. Can you C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,5 +7,5 @@ def no_c(my_string):
         if my_string[i] == 'c' or my_string[i] == 'C':
             continue
         else:
-            new_string[i] += my_string[i]
+            new_string += my_string[i]
     return (new_string)
