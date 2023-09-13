3f38a55ab5378b36d683b64afc6287b5c7cb9ad2
Habeeb <tijanihabeeb12@outlook.com>
2023-09-13 22:46:44 +0100

    Add: 10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 def best_score(a_dictionary):
     """returns a key with the biggest integer value"""
+    if not a_dictionay:
+        return (None)
     keys = sorted(a_dictionary)
     max_val = a_dictionary[keys[0]]
     max_key = keys[0]
