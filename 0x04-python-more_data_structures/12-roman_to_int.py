75e2c1ed325e7e7d76d5fe136802e27167b6ca49
Habeeb <tijanihabeeb12@outlook.com>
2023-09-14 13:05:27 +0100

    Add: 12. Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,\
+            'M': 1000}
+    roman_len = len(roman_string)
+    if roman_len == 1:
+        return (roman_dict[roman_string])
+
+    num = 0
+    for i in range(roman_len):
+        if roman_string[i] == "I":
+            if i+1 != roman_len and roman_string[i+1] == "V":
+                i += 1
+                num += 4
+            elif i+1 != roman_len and roman_string[i+1] == "X":
+                i += 1
+                num += 9
+            else:
+                num += 1
+        elif roman_string[i] == "X":
+            if i+1 != roman_len and roman_string[i+1] == "L":
+                i += 1
+                num += 40
+            elif i+1 != roman_len and roman_string[i+1] == "C":
+                i += 1
+                num += 90
+            else:
+                num += 10
+        elif roman_string[i] == "C":
+            if i+1 != roman_len and roman_string[i+1] == "D":
+                i += 1
+                num += 400
+            elif i+1 != roman_len and roman_string[i+1] == "M":
+                i += 1
+                num += 900
+            else:
+                num += 100
+        elif  roman_string[i] == "V":
+            num += 5
+        elif roman_string[i] == "L":
+            num += 50
+        elif roman_string[i] == "D":
+            num += 500
+        elif roman_string[i] == "M":
+            num += 1000
+    return (num)
