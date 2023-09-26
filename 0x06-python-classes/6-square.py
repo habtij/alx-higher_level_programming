f2ca4ece720907cb26a97480540a806c63fdce00
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 20:26:26 +0100

    Add: 6. Coodinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,7 +49,8 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                print(" " * self.__position[0], end="")
+                if self.__position[1] < 0:
+                    print(" " * self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
