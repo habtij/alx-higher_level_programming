db98a22471ab2d31fff36e870c2b46398e544b4d
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 20:28:03 +0100

    Add: 6. Coodinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,12 +45,14 @@ class Square:
 
     def my_print(self):
         """Prints the square with the character #"""
+        if self.__position[1] > 0:
+            print("")
+
         if self.__size == 0:
             print("")
         else:
             for i in range(self.__size):
-                if self.__position[1] < 0:
-                    print(" " * self.__position[0], end="")
+                print(" " * self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
