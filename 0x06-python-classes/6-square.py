a684d4a6f3615b30cfdae6a135c6cd9833abb2a7
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 20:07:21 +0100

    Add: 6. Coodinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""The module creates a square class with size attribute, area,
+size getter & setter, my_print and position getter and setter method
+"""
+
+
+class Square:
+    """A square class that contains private size and position attribute,
+    size and position getter and setter, my_print and area method
+    """
+    def __init__(self, size=0, position=(0,0)):
+        """Initiliaze size and position attribute for square"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Returns the size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size for square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Returns the value of the position for square"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """Sets the value of the position for square"""
+        if not isinstance(value, tuple) and (value[0] < 0 and value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                if position[1] <= 0:
+                    print(" " * self.__position[0], end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
