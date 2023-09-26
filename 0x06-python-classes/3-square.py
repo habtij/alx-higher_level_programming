31c620a2bba423a0942602ebc45ad1b67b0c97db
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 18:17:39 +0100

    Add: 3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The module creates a square class with size attribute and area method"""
+
+
+class Square:
+    """A square class with private attribute size and method area"""
+    def __init__(self, size):
+        """Initialize square attribute size"""
+        if isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the area of square"""
+        return (self.__size ** 2)
