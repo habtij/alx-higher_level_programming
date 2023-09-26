49accb76c8ebf52f15fcfc7c50e28c4f0cd39306
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 15:16:07 +0100

    Add: 2. Sze validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The module creates a square class with size attribute"""
+
+
+class Square:
+    """A square class with size attribute"""
+    def __init__(self, size=0):
+        """Initialize the private size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
