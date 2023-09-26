810711276f02804eacc1aaddb16c7b08f73bf170
Habeeb <tijanihabeeb12@outlook.com>
2023-09-26 14:21:09 +0100

    Add: 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a class that defines a square"""
+
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size):
+        """Instantiate with a size"""
+        self.__size = size
