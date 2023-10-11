5ff5f5b99535c99b4415b6eda6faf7e3091b134d
Habeeb <tijanihabeeb12@outlook.com>
2023-10-11 22:20:51 +0100

    Add: 13. Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a function ``append_after``"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line containing a
+    specific string
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text _= new_string
+
+    with open(filename, "w") as w:
+        w.write(text)
