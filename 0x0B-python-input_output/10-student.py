c43109323a0d10d83e5d2a7798c90eea9e09795f
Habeeb <tijanihabeeb12@outlook.com>
2023-10-11 21:56:06 +0100

    Update: 10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,6 +18,6 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance"""
-        if type(attrs) == list and all(type(ele) == str for ele in attrs)):
+        if type(attrs) == list and all(type(ele) == str for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
