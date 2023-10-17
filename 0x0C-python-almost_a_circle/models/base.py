ea1b87b2b9e07272ee1da63335c2b0d6483923fa
Habeeb <tijanihabeeb12@outlook.com>
2023-10-17 21:13:12 +0100

    Update: 1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,8 @@ class Base:
         __nb_objects (int): Number of objects created (private class attr)
         id (int): Public instance attribute
     Method:
-        __init__ (id): Initializes a object with given id or None
+        __init__(id): Initializes a object with given id or None
+        integer_validator(name, value): validates the value
     """
     __nb_objects = 0
 
@@ -26,3 +27,10 @@ class Base:
         else:
             self.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def integer_validator(self, name, value):
+        """Validates the value, and raise an exception if there is error"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value < 0:
+            raise ValueError("{} must be > 0".format(name))
