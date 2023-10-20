57800350f584734b627027f739f75b4f82df72eb
Habeeb <tijanihabeeb12@outlook.com>
2023-10-20 23:53:35 +0100

    Update: 10, And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a ``Square`` and inherits from ``Rectangle``"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
