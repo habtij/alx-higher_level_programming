08a8576642fc4852bc55275ca5d334d5f8af2eba
Habeeb <tijanihabeeb12@outlook.com>
2023-11-15 22:05:30 +0100

    Add: 9. Cities by states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Liss all cities contained in the database `hbtn_0d_usa`
+SELECT cities.id, cities.name, state.name
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
+ORDER BY cities.is;
