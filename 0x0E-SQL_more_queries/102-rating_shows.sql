07bbfbbf6221ec9ee5b51a53cbe9999121c499f8
Habeeb <tijanihabeeb12@outlook.com>
2023-11-16 21:34:45 +0100

    Update: 19. Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
--- Import the database hbtn_0d_tvshows dump to your MySQL server
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_showss.id
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
 ORDER BY rating DESC;
