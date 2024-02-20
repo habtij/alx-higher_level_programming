15207a58e59051f3c750e9f2681694235ef69653
Habeeb <tijanihabeeb12@outlook.com>
2024-02-20 18:53:29 +0100

    Update: 4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
-    console.log(result.reduce((count, movie) => {
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endswith('/18/'))
       ? count + 1 : count;
     }, 0));
