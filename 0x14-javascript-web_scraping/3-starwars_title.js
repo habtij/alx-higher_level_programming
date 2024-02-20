747eedb4e9f0b7285a5d239f855b79c64d986fb9
Habeeb <tijanihabeeb12@outlook.com>
2024-02-20 18:16:50 +0100

    Update: 3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/";
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 const num = process.argv[2];
 
 request(url + num, function (error, response, body) {
