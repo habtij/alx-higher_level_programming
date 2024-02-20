67f7f3e24f8c649490f49d0c893ad4cbc9195b19
Habeeb <tijanihabeeb12@outlook.com>
2024-02-20 18:05:28 +0100

    Add: 3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const url = "https://swapi-api.alx-tools.com/api/films/";
+const num = process.argv[2];
+
+request(url + num, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const resp = JSON.parse(body);
+    console.log(resp.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
