7e63d3ba3f14094741af878e128216ca583f27fd
Habeeb <tijanihabeeb12@outlook.com>
2024-03-08 12:26:11 +0100

    Update: 7. Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let nums = 0;
   for ( let i = 0; i < list.length; i++) {
-    if (searchElement == list[i]) {
+    if (searchElement === list[i]) {
       nums++;
     }
   }
