6a4e34f0537d81537fb2dde0cfae897f91c48eb0
Habeeb <tijanihabeeb12@outlook.com>
2024-03-08 12:11:32 +0100

    Add: 6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square_parent = require('./5-square');
+
+class Square extends Square_parent {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let res = '';
+      for (j = 0; j < this.width; j++) {
+        res += c;
+      }
+      console.log(res);
+    }
+  }
+}
+
+module.exports = Square;
